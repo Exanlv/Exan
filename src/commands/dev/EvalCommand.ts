e44f87034396fa9ffb77579e415d78c660c64b5b
Exan <exan@landviz.nl>
2020-07-28 22:03:53 +0200

    Splice when it should be slice

--- src/commands/dev/EvalCommand.ts
+++ src/commands/dev/EvalCommand.ts
@@ -5,7 +5,7 @@ export class EvalCommand extends BaseDevCommand {
 		const args = this.args.splice(1, this.args.length - 1);
 
 		try {
-			const res = (eval(args.join(' '))).splice(0, 1990);
+			const res = String((eval(args.join(' ')))).slice(0, 1990);
 
 			await this.reply(`\`\`\`${res}\`\`\``);
 		} catch (e) {

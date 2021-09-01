7dc48cea4d93c1097052ac4847e8146f4cdafff1
KristN <kristian.kunc@gmail.com>
2021-09-01 21:24:50 +0200

    added test response for slash commands

--- src/handle_application_command.ts
+++ src/handle_application_command.ts
@@ -4,5 +4,16 @@ import { Translator } from 'classes/Translator';
 import { Client } from 'eris';
 
 export async function handle_application_command(command: ApplicationCommand, bot: Client, translator: Translator, mongo_db: MongoDB) {
-    await bot.createMessage(command.channel_id, "hi");
+    const method = 'POST';
+    const path = `/interactions/${command.id}/${command.token}/callback`;
+    const auth = true;
+    const body = {
+        "type": 4,
+        "data": {
+            "content": "test response"
+        }
+    }
+    const file = null
+
+    bot.requestHandler.request(method, path, auth, body, file);
 }
